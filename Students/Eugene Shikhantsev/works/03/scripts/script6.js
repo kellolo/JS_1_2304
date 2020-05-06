26baa79ef89d0fca4942adb19a486db88000eab9
sk8work <eshikhantsev@gmail.com>
2020-05-06 14:21:56 +0300

    almost

--- Students/Eugene Shikhantsev/works/03/scripts/script6.js
+++ Students/Eugene Shikhantsev/works/03/scripts/script6.js
@@ -1,1 +1,14 @@
 // 6. Создать игру "Быки и коровы", используя доступные инструменты
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+computer = new Array();
+for (let i = 1; i <= 4;){
+    let newNum = Math.round(Math.random(9)*10);
+    for (let idx = 0; idx <= computer.length; idx++) {
+        if newNum == computer[idx] {
+
+        }
+        computer.push(newNum);
+    }
+    i++;
+}
